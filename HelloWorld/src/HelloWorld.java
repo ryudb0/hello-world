4cb26ef6cb73edc87553898c4b77a5c53e79ba1d
ryudb <ryudb@DESKTOP-FLBB9CS>
2023-01-10 20:13:44 +0900

    내 인생의 공장은 이게 처음이자 마지막이다.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("i'm starving now");
+		System.out.println("i want to get out on factory");
 	}
 	
 }
